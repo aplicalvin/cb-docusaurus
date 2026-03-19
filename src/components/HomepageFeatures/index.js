22a21542fa496ab6836c13bf2ab6d262527b4f00
ApliCalvin <thecalvins.sims@gmail.com>
2026-03-19 23:26:30 +0700

    setting config

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,49 +1,71 @@
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
+import Link from '@docusaurus/Link';
 import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Materi Terstruktur',
+    title: 'Web Basic',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Belajar mulai dari sintaks paling dasar, hingga konsep lanjutan C++
-        seperti Object-Oriented Programming, Pointer, dan Manajemen Memori.
+        Mulai petualanganmu dari fundamental Web Development. Pelajari HTML, CSS, dan Javascript dari awal!
       </>
     ),
+    link: '/web-basic/intro',
+    linkText: 'Mulai Quest Web 🌐',
   },
   {
-    title: 'Contoh Kode Praktis',
+    title: 'Algorithm',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Setiap topik dilengkapi dengan contoh program (source code) yang
-        dapat langsung Anda salin, jalankan, dan modifikasi sendiri.
+        Asah logikamu dengan memecahkan berbagai teka-teki algoritma. Persiapkan dirimu untuk tantangan nyata!
       </>
     ),
+    link: '/algorithm/intro',
+    linkText: 'Mulai Quest Algo 🧠',
   },
   {
-    title: 'Fokus pada Problem Solving',
+    title: 'C++ Basic',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Kembangkan logika pemrograman Anda dengan berlatih menggunakan
-        studi kasus dan penyelesaian masalah yang relevan di dunia nyata.
+        Masuki dungeon C++ dan pelajari bahasa pemrograman inti yang sangat kuat, mulai dari syntax hingga memori.
       </>
     ),
+    link: '/cpp-basic/intro',
+    linkText: 'Mulai Quest C++ ⚔️',
+  },
+  {
+    title: 'C++ Basic',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Masuki dungeon C++ dan pelajari bahasa pemrograman inti yang sangat kuat, mulai dari syntax hingga memori.
+      </>
+    ),
+    link: '/cpp-basic/intro',
+    linkText: 'Mulai Quest C++ ⚔️',
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description, link, linkText }) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
+    <div className={clsx('col col--4', 'margin-bottom--lg')}>
+      <div className="card padding--lg" style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+        <div className="text--center">
+          <Svg className={styles.featureSvg} role="img" />
+        </div>
+        <div className="text--center padding-horiz--md" style={{ flexGrow: 1 }}>
+          <Heading as="h3">{title}</Heading>
+          <p>{description}</p>
+        </div>
+        <div className="text--center margin-top--md">
+          <Link className="button button--primary button--block" to={link}>
+            {linkText}
+          </Link>
+        </div>
       </div>
     </div>
   );
@@ -51,7 +73,7 @@ function Feature({Svg, title, description}) {
 
 export default function HomepageFeatures() {
   return (
-    <section className={styles.features}>
+    <section id="quests" className={styles.features} style={{ paddingTop: '4rem', paddingBottom: '4rem' }}>
       <div className="container">
         <div className="row">
           {FeatureList.map((props, idx) => (
